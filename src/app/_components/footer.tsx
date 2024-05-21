75dd283670d0a1aad18ce1e089df6f5371c6b3cc
shreyash <shreyash_c@yahoo.com>
2024-05-21 22:50:28 +0530

    chore: Update footer styling and image gallery layout

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -3,7 +3,7 @@
 export function Footer() {
   return (
     <>
-      <div className="mt-7 flex w-full flex-col items-center justify-center p-5 text-sm text-slate-400  shadow-sm shadow-slate-300">
+      <div className="mt-10 flex w-full flex-col items-center justify-center p-5 text-sm text-slate-400  shadow-sm shadow-slate-300">
         <p className="text-xs">Design & Build By</p>
         <h1>
           <a href="https://x.com/shreyashchandra">@shreyashchandra</a>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,24 +16,21 @@ export default async function HomePage() {
   });
 
   return (
-    <main>
-      <div className="relative h-full w-full bg-black">
-        <div className="absolute left-0 right-0 top-[-10%] h-[1000px]  w-full rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]">
-          <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
-            {images.map((image) => (
-              <div key={image.id} className="duration-200 hover:scale-110">
-                <img
-                  src={image.url}
-                  alt="image"
-                  className="h-auto max-w-full rounded-lg"
-                />
-              </div>
-            ))}
-          </div>
-
-          <Footer />
+    <div className="relative h-full w-full bg-black">
+      <div className="absolute left-0 right-0 top-[-10%] h-[1000px]  w-full rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]">
+        <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
+          {images.map((image) => (
+            <div key={image.id} className="duration-200 hover:scale-110">
+              <img
+                src={image.url}
+                alt="image"
+                className="h-auto max-w-full rounded-lg"
+              />
+            </div>
+          ))}
         </div>
+        <Footer />
       </div>
-    </main>
+    </div>
   );
 }
